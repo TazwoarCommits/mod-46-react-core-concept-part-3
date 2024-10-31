501c0264fc6810c2fdb19b41ccf43b33fd1cd369
TazwoarCommits <tazwoarlives@gmail.com>
2024-10-31 14:25:48 +0600

    46.1

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,30 @@
 
 
 const SimpleForm = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.text.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+
+        console.log('Form Submitted')
+    }
+
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit} >
+                <input type="text" name="text"/>
+                <br />
+                <br />
+                <input type="email" name="email" />
+                <br />
+                <br />
+                <input type="text" name="phone" />
+                <br />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
